605f797a492515ad1baa8fd09e4961dc559a868c
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2022-03-31 11:28:53 +0700

    Add building page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,12 @@
 <script context="module">
-	export const prerender = true;
 </script>
 
 <script>
 </script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
 <section>
 
+	Building...
 
 </section>
 
